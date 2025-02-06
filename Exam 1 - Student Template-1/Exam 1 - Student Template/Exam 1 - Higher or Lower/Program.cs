4edb30bc12c5c4af54c55186a3ba9d2326b02eaf
sc0981772 <sc0981772@otc.edu>
2025-02-06 09:28:53 -0600

    commit 1

---
+++ Exam 1 - Student Template-1/Exam 1 - Student Template/Exam 1 - Higher or Lower/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace LargestNumberFinder
+{
+    public class NumberProgram
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int number;
+            int largest = int.MinValue //Initialize at the smallest possible integer
+
+            //User prompt
+            Console.WriteLine("Please enter 10 positive integers.");
+
+            while (counter < 10)
+            {
+                Console.WriteLine($"Enter integer {counter + 1}: ");
+                ProcessInput(); //calls the ProcessInput function which also has the ReadLine embedded in, hopefully did this correctly
+                try
+                {
+                    Console.WriteLine($"You entered: {number}");
+                    //negative number course correction
+                    if (number <= 0) 
+                    {
+                        Console.WriteLine("Error: Please enter a positive integer.");
+                        break; //does not continue to next part of the loop
+                    }
+
+                    //zero course correction
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Error: Please enter a positive integer.");
+                        break; //does not continue to next part of the loop
+                    }
+
+                    counter++;
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Invalid input. Please enter a valid integer.");
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occured: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine($"The largest number is {largest}");
+        }
+
+        public static int ProcessInput(string input, ref int largest)
+        {
+            input = int.Parse(Console.ReadLine());
+            if (input > largest)
+            {
+                largest = input;
+            }
+        }
+    }
+}
