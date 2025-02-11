9096808175c8bb4cfa7ba0d310aa1c91ee172fe1
sc0981772 <sc0981772@otc.edu>
2025-02-11 12:23:12 -0600

    catch blocks assignment

---
+++ Module5CatchBlocks/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module5CatchBlocks
+{
+    using System;
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "49"; // or use "49" in a notebook
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you entered is not a valid number format.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+        }
+    }
+}
