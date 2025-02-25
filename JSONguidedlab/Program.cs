0fb184fe6f5626fb59699a4c1b164b2ec01b60a6
sc0981772 <sc0981772@otc.edu>
2025-02-25 09:58:35 -0600

    intro to JSON

---
+++ JSONguidedlab/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JSONguidedlab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person { Name = "John Doe", Age = 30 },
+                new Person { Name = "Jane Smith", Age = 25 },
+                new Person { Name = "Mike Johnson", Age = 40 }
+            };
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(people);
+                Console.WriteLine("Serialized JSON: " + json);
+
+                List<Person> deserializedPeople = JsonConvert.DeserializeObject<List<Person>>(json);
+                Console.WriteLine("Deserialized People:");
+                foreach (Person person in deserializedPeople)
+                {
+                    Console.WriteLine("Name - " + person.Name + ", Age - " + person.Age);
+                }
+            }
+            catch (JsonException jsonEx)
+            {
+                Console.WriteLine("An error occurred while processing JSON: " + jsonEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+
+            //string json = JsonConvert.SerializeObject(person);
+            //Console.WriteLine("Serialized JSON: " + json);
+
+            //Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+            //Console.WriteLine("Deserialized Person: Name - " + deserializedPerson.Name + ", Age - " + deserializedPerson.Age);
+        }
+    }
+}
