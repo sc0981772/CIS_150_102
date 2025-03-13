5a05c1bb947b08500cd1c6ffbcb39be6b911d7fa
sc0981772 <sc0981772@otc.edu>
2025-03-13 02:57:15 -0500

    pokemon api

---
+++ PokemonAPIapp/PokeApiService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PokemonAPIapp
+{
+    public class PokeApiService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<Pokemon> GetPokemonAsync(string nameOrId)
+        {
+            try
+            {
+                string url = $"https://pokeapi.co/api/v2/pokemon/{nameOrId}";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Error: Unable to fetch Pokémon data.");
+                    return null;
+                }
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(jsonResponse);
+                return pokemon;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ PokemonAPIapp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PokemonAPIapp
+{
+    public class Program
+    {
+        private static List<Pokemon> requestedPokemonList = new List<Pokemon>();
+
+        public static async Task Main(string[] args)
+        {
+            PokeApiService pokeApiService = new PokeApiService();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a Pokémon name or ID (or type 'exit' to quit):");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                    break;
+
+                Pokemon pokemon = await pokeApiService.GetPokemonAsync(input);
+
+                if (pokemon != null)
+                {
+                    requestedPokemonList.Add(pokemon);
+                    Console.WriteLine($"Name: {pokemon.Name}, Height: {pokemon.Height}, Weight: {pokemon.Weight}");
+                }
+
+                Console.WriteLine("Previously Requested Pokémon:");
+                foreach (var p in requestedPokemonList)
+                {
+                    Console.WriteLine($"- {p.Name}");
+                }
+            }
+        }
+    }
+}
